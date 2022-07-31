b84b7a79454f41147f89e777912fbd12a07ce581
MuksithMRA <muksithmra@gmail.com>
2022-07-31 19:31:00 +0530

    delete burger added

---
+++ delete_burger.php
@@ -0,0 +1,22 @@
+<?php
+$id = $_GET["id"];
+
+//Create Connection
+$conn = new mysqli("localhost", "root", "", "burger_house");
+
+//Check if connection success
+if($conn->connect_error){
+    die("Connection failed : " . $conn->connect_error);
+}
+
+//Execute query
+$sql = "DELETE FROM burgers WHERE id='$id'";
+$result = $conn->query($sql);
+
+if($result>0){
+    echo "Deleted Successfully";
+}else{
+    echo "Deletation Error";
+}
+
+?>
--- view_burger.php
+++ view_burger.php
@@ -3,13 +3,16 @@
 //Create Connection
 $conn = new mysqli("localhost", "root", "", "burger_house");
 
+//Check if connection success
 if($conn->connect_error){
     die("Connection failed : " . $conn->connect_error);
 }
 
+//Execute query
 $sql = "SELECT * FROM burgers";
 $result = $conn->query($sql);
 
+//fetch data
 if($result->num_rows>0){
     while($row = $result->fetch_assoc()){
         $isAvailable = $row["isAvailable"] ? "Available" : "Not Available";
@@ -25,7 +28,7 @@ if($result->num_rows>0){
                         </p>
                     </div>
                     <div class="card-footer">
-                        <button class="btn btn-Danger">Delete</button>
+                        <a href="./delete_burger.php?id='.$row["id"].'" role="button" class="btn btn-Danger">Delete</a>
                     </div>
                 </div>
             </div>';
